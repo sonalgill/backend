62e363597baf5672226703e359cf39eafd95408e
sonalgill <sonalgill37@gmail.com>
2022-08-06 00:10:17 +0530

    update

---
+++ src/logger/logger.js
@@ -0,0 +1,9 @@
+const welcome1 = "Plutonium."
+
+let welcome = function(){
+console.log('Welcome to my application. I am Sonal Gill and a part of FunctionUp Plutonium cohort and batch name is', welcome1)
+}
+
+module.exports.velu = welcome1
+module.exports.welcome = welcome
+
