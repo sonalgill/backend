d78a0c64a7700d663f545354e8388aa98c2fd5e8
sonalgill <sonalgill37@gmail.com>
2022-08-08 21:59:41 +0530

    routes/route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const welpath = require('../logger/logger') 
 const cdate = require('../util/helper')
 const ttrim = require('../validator/formatter')
-const _ = require('lodash')
+const _ = require('lodash');
+const { json } = require('body-parser');
+const { indexOf } = require('lodash');
 
 
 router.get('/test-me', function (req, res) {
@@ -35,26 +37,72 @@ router.get('/test-me', function (req, res) {
     let obj = _.fromPairs(arr4)
     console.log(obj)
 
-router.get('/details-s', function(req, res){
-    let studentDetails = ['Sonal', 'Tanuj', 'Arpit']
-    res.send("fine")
+});
+
+router.get('/get/movies', function(req, res){
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movies)
 })
 
-    // router.get('/students-detail/:name', function(req, res){
-    //     let reqParams = req.params
-    //     let studentName = reqParams.name
-    //     console.log('name is => ', studentName)
-    //     let studentDetail = studentName
-
-    //     res.send(studentDetail)
-    // })
 
 
-});
+router.get('/get-movie/:indexNumber', function(req, res){
+    let moviearr = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    let mName = req.params.indexNumber
+    if(mName >= moviearr.length){
+        res.send('Please use valid index')
+    }else {
+        res.send(moviearr[mName])
+    }
+})
+
+router.get('/get/films', function(req, res){
+    let objectarr = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }
+    ]
+    res.send(objectarr)
+})
+
+router.get('/get/films/:filmId', function(req, res){
+    let objarr =
+    [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }
+    ]
+    
+       let filmName = req.params.filmId
+       for(i = 0; i < objarr.length; i++){if(filmName == objarr[i].id){
+        res.send(objarr[i])
+       }}  {
+        res.send('No movie exists with this id')
+       }      
+})
+
 
 
 router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
+    res.send('This is the second routes implementation ')
 })
 
 router.get('/give-me-students-data',function(req, res){
