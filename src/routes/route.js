49112ed67c960eafd0ce1d9c7aab0da06f63f26d
sonalgill <sonalgill37@gmail.com>
2022-08-10 00:06:32 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,24 +2,6 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/sol1', function (req, res) {
-    let arr = [1, 2, 3, 5, 6, 7]
-    let n = arr[arr.length - 1]
-    let sum = arr.reduce((ini, curr) => ini + curr)
-    let missingNumber = (n * (n + 1) / 2) - sum
-    res.send({ data: missingNumber })
-})
-
-router.get('/sol2', function (req, res) {
-    let arr = [33, 34, 35, 37, 38]
-    let n = arr.length + 1
-    let last = arr[arr.length - 1]
-    let sum = arr.reduce((ini, curr) => ini + curr)
-    let missingNumber = (n*(arr[0] + last)/2) - sum
-    res.send({data : missingNumber})
-})
-
-
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
